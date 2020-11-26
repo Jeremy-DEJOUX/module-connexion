849de2e91091d76a5c5bf1bdb3de364815104c22
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-11-26 16:46:42 +0100

    ajout de bootstrap

--- pages/admin.php
+++ pages/admin.php
@@ -13,6 +13,7 @@ if ($_SESSION['login'] == 'admin') {
     <link rel="stylesheet" href="../CSS/header.css">
     <link rel="stylesheet" href="../CSS/footer.css">
     <link rel="stylesheet" href="../CSS/admin.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title></title>
   </head>
   <body>
--- pages/profil.php
+++ pages/profil.php
@@ -22,8 +22,8 @@ if (isset($_SESSION['id'])){
     <title>Profil</title>
     <link rel="stylesheet" href="../CSS/profil.css">
     <link rel="stylesheet" href="../CSS/header.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="../CSS/footer.css">
-    <script src="https://kit.fontawesome.com/56188ecd90.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <!-- ===================================HEADER========================================== -->
