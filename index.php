c6e16ca963ec152a599ab460e85c618c09371a1f
DEJOUX Jérémy <jeremy.dejoux@laplateforme.io>
2020-12-01 11:30:11 +0100

    ajout d'image de presentation et de personnage

--- index.php
+++ index.php
@@ -15,26 +15,74 @@ require_once('configs/config.php');
     <link rel="stylesheet" href="CSS/footer.css">
     <link rel="stylesheet" href="CSS/index.css">
   </head>
-  <body>
+  <body class="flex column">
     <?php require_once('pages/header.php'); ?>
 
     <main class="" id="main_index">
-      <section class="" id="">
-
+      <section class="flex a_center j_center" id="Paralax_Friend">
+        <h1>FRIENDS</h1>
       </section>
+
+
+
+      <section class="flex column j_around a_center" id="Personnage">
+        <article class="flex j_around a_center">
+          <div class="flex column a_center" id="Monica">
+            <div class="">
+
+            </div>
+            <h2>Monica</h2>
+          </div>
+
+          <div class="flex column a_center" id="Chandler">
+            <div class="">
+
+            </div>
+            <h2>Chandler</h2>
+          </div>
+
+
+          <div class="flex column a_center" id="Joey">
+            <div class="">
+
+            </div>
+            <h2>Joey</h2>
+          </div>
+
+          <div class="flex column a_center" id="Phoebe">
+            <div class="">
+
+            </div>
+            <h2>Phoebe</h2>
+          </div>
+
+        </article>
+
+
+
+        <article class="flex j_around a_center">
+          <div class="flex column a_center" id="Rachel">
+            <div class="">
+
+            </div>
+            <h2>Rachel</h2>
+          </div>
+
+          <div class="flex column a_center" id="Ross">
+            <div class="">
+
+            </div>
+            <h2>Ross</h2>
+          </div>
+        </article>
+      </section>
+
 
 
 
       <section>
 
-      </section>
 
-
-
-
-      <section>
-
-        
       </section>
     </main>
 
